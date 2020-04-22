6cac45392c727ac269db1688639dc27a861c2a11
barhouum7 <bb.med2000@gmail.com>
2020-04-22 09:00:42 +0100

    Completed The Shuffle Function.

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,7 +18,32 @@ const gameBlocks = Array.from(blocksContainer.children)
 
 const gBlocksOrderRange = [...Array(gameBlocks.length).keys()]
 
+// console.log(gBlocksOrderRange)
+shuffle(gBlocksOrderRange)
+// console.log(gBlocksOrderRange)
+
 // Add Order CSS Property To Game Blocks.
 gameBlocks.forEach((block, index) => {
     block.style.order = gBlocksOrderRange[index]
 })
+
+// Shuffle Function
+function shuffle(array){
+    // Settings Vars
+    let current = array.length, temp, random
+
+    while (current > 0){
+        // Get The Random Number
+        random = Math.floor(Math.random() * current)
+        //Decrease The Array's Length By One
+        current--
+
+        // [1] Save Current Element In Stash
+        temp = array[current]
+        // [2] Current Element = Random Element
+        array[current] = array[random]
+        // [3] Random Element = Get Element From Stash
+        array[random] = temp
+    }
+    return array
+} 
