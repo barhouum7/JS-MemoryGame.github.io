4ce26f86a6799c1f0198f9a42968a97d1ed0ee01
barhouum7 <bb.med2000@gmail.com>
2020-04-23 23:45:26 +0100

    Completed The Shuffle, The flip Block, The Stop Clicking AND The Matched Blocks Functions.

--- assets/js/main.js
+++ assets/js/main.js
@@ -25,7 +25,64 @@ shuffle(gBlocksOrderRange)
 // Add Order CSS Property To Game Blocks.
 gameBlocks.forEach((block, index) => {
     block.style.order = gBlocksOrderRange[index]
+
+    // Add A Click Event
+    block.addEventListener('click', () => {
+        // Trigger The flipBlock Function
+        flipBlock(block)
+    })
 })
+
+// Add Flip Block Function
+function flipBlock(selectedBlock) {
+    // Add Class is-flipped
+    selectedBlock.classList.add('is-flipped')
+    // Collect All Flipped Cards
+    let allFlippedBlocks = gameBlocks.filter(flippedBlock => flippedBlock.classList.contains('is-flipped'))
+    // Checking If There's Two Selected Blocks THEN Stop It And Check.
+    if (allFlippedBlocks.length === 2){
+        // console.log('Two Cards Selected !')
+
+        // Adding Stop Clicking Function
+        stopClicking()
+
+        // Adding Check Matched Block Function
+        checkMatchedBlocks(allFlippedBlocks[0], allFlippedBlocks[1])
+    }
+
+}
+
+// Adding Stop Clicking Function
+function stopClicking() {
+    // Adding Class No Clicking On Main Container
+    blocksContainer.classList.add('no-clicking')
+
+    // Removing Class No Clicking After A Specific Duration
+    setTimeout(() => {
+        blocksContainer.classList.remove('no-clicking')
+    }, duration)
+}
+
+function checkMatchedBlocks(firstBlock, secondBlock) {
+    let triesElement = document.querySelector('.tries span')
+
+    if (firstBlock.dataset.technology === secondBlock.dataset.technology){
+        
+        firstBlock.classList.remove('is-flipped')
+        secondBlock.classList.remove('is-flipped')
+
+        firstBlock.classList.add('is-match')
+        secondBlock.classList.add('is-match')
+
+    }else {
+        triesElement.innerHTML = parseInt(triesElement.innerHTML) +1
+
+        setTimeout(() => {
+            firstBlock.classList.remove('is-flipped')
+            secondBlock.classList.remove('is-flipped')
+        }, duration)
+    }
+}
 
 // Shuffle Function
 function shuffle(array){
